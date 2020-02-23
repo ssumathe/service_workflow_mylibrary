5f1c1a02af0c510e2d4c38834a350e4060a3bf79
ssumathe <ssumathe@gmail.com>
2020-02-23 09:06:56 -0500

    Update buildNode.groovy

--- vars/buildNode.groovy
+++ vars/buildNode.groovy
@@ -3,9 +3,11 @@ def config = [:]
 body.resolve.Strategy=Clousure.DELEGATE_FIRST
 body.delegate= config
 body()
+  node {
 def folder ="my folder in build groovy file"
 def buildCommands = config.buildC
   buildJava(buildCommands)
+}
 }
 def buildJava(buildCommands) {
   stage("Application build") {
