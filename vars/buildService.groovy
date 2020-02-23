335b22ad330d94c7a2814e57102ec8bcf3cd62d0
ssumathe <ssumathe@gmail.com>
2020-02-23 09:05:48 -0500

    Update buildService.groovy

--- vars/buildService.groovy
+++ vars/buildService.groovy
@@ -4,14 +4,13 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    node {
+    
     def folderName = "MyFolder"
     def buildCommands = config.buildCommands
     println "folderName: " + folderName
     println "buildCommand is " + buildCommands
-    }
 
     buildNode {
         buildC = buildCommands
         }
-}    
+}  
