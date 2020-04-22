a898e860b8bb31cbf95075942edd797b3d612435
ssumathe <ssumathe@gmail.com>
2020-04-22 01:10:20 -0400

    update file with json content

--- vars/buildService.groovy
+++ vars/buildService.groovy
@@ -5,4 +5,36 @@ def call(body) {
     body()
     def folderName = "MyFolderWorking"
     println "folderName: " + folderName
+    
+    def json = new JsonSlurperClassic().parseText(
+    """
+    {
+    	  "apiVersion": "v1",
+    	  "kind": "Service",
+    	  "metadata": {
+    	    "name": "${deployName}",
+    	    "namespace": "${config.namespace}",
+    	    "annotations": {
+    	        "prometheus.io/scrape": "true",
+    	        "prometheus.io/path": "${externalContextPath}/prometheus"
+    	    },
+    	    "labels": {
+    	      "context": "${config.context}",
+    	      "hystrix.enabled": "${isHystrixEnabled}",
+    	      "hystrix.cluster": "default"
+    	    }
+    	  },
+    	  "spec": {
+    	    "ports": [
+    	      {
+    	    	"name": "http",
+    	        "port": 8080,
+    	        "targetPort": ${appPort}
+    	      }
+    	    ],
+    	    "selector": {
+    	      "app": "${deployName}"
+    	    }
+    	  }
+    	}
 }
