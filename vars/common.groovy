3633c98d78d1bd57d8a8ee32d690be1eb443d79e
ssumathe <ssumathe@gmail.com>
2020-04-22 03:57:47 -0400

    Create common.groovy

---
+++ vars/common.groovy
@@ -0,0 +1,10 @@
+ import groovy.json.JsonBuilder
+ 
+ def writeJSONFile(fileName, json) {
+        def builder = new JsonBuilder(json)
+        String output = builder.toPrettyString()
+	echo output
+	//echo 'writing resource '+'service.json' + ':' '\n' + output
+	writeFile file: fileName, text: output
+    
+}
